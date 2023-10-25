519180bb9556ab22cf9a99f92aabd0295cc47eae
esixce <98708432+esixce@users.noreply.github.com>
2023-10-25 10:42:59 -0700

    second commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// Initialize Rellax
+const rellax = new Rellax('.parallax .circle', {
+    speed: -2, // Adjust the speed value as needed
+});
+
+// You can create multiple instances with different speed values if needed
+// const rellax2 = new Rellax('.other-elements', {
+//     speed: -1,
+// });
+
+// Adjust the speed value as needed to control the parallax effect
